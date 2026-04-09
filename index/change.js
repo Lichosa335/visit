2e3a3cfce063b9a82fcf21cac9b2c645f9510780
Max <maxanc2009@gmail.com>
2026-04-09 23:02:41 +0800

    обновление сайта

--- index/change.js
+++ index/change.js
@@ -1,66 +1,4 @@
-/*Переключение слайдов регистрации*/
-
-document.getElementById('log').addEventListener('click', function() {
-    const log_button = document.getElementById('log');
-    const naim = document.getElementById('naim');
-    const log_body = document.getElementById('log_body');
-    const state = this.dataset.state;
-    const sig_body = document.getElementById('sig_body')
-
-    /*переключение кнопкт назад и войти*/
-
-    if (state === 'off'){
-        this.dataset.state = 'on'
-        naim.classList.add('no_vid');
-        log_body.classList.add('vid');
-        log_body.classList.remove('no_vid');
-        log_button.textContent = 'Remove';
-        this.style.background = '#ff4444';
-
-    }else {
-        this.style.background = '#11E200';
-        this.dataset.state = 'off';
-        this.textContent = 'login';
-        naim.classList.add('vid');
-        naim.classList.remove('no_vid');
-        log_body.classList.add('no_vid');
-        log_body.classList.remove('vid');
-        sig_body.classList.add('no_vid');
-        sig_body.classList.remove('vid');
-
-    }
-});
-
-/*кнопка регистрации в поле входа*/
-
-document.getElementById('log_ent').addEventListener('click', function() {
-    const log_body = document.getElementById('log_body');
-    const sig_body = document.getElementById('sig_body');
-    const state = this.dataset.state;
-
-        sig_body.classList.remove('vid');
-        sig_body.classList.add('no_vid');
-        log_body.classList.add('vid');
-        log_body.classList.remove('no_vid');
-
-
-
-});
-
-/*кнопка входа в поле регистрации*/
-
-document.getElementById('sig_ent').addEventListener('click', function() {
-    const sig_body = document.getElementById('sig_body');
-    const log_body = document.getElementById('log_body');
-
-        log_body.classList.remove('vid');
-        log_body.classList.add('no_vid');
-        sig_body.classList.remove('no_vid');
-        sig_body.classList.add('vid');
-});
-
 /*Переключение главной и поля выбора курсов*/
-
 document.getElementById('cou').addEventListener('click', function(){
     const cours_body = document.getElementById('cours_body');
     const mein = document.getElementById('mein');
@@ -74,7 +12,7 @@ document.getElementById('cou').addEventListener('click', function(){
         const btn = document.getElementById(id);
         if (btn) {
             btn.classList.add('no_dis');
-            btn.classList.remove('vid');
+
         }
     });
 
@@ -94,11 +32,13 @@ document.getElementById('cou').addEventListener('click', function(){
     span.setAttribute('type', 'button');
 
 });
+
 /*Переключение поля выбора курсов и главной */
 document.getElementById('span').addEventListener('click', function(){
     const cours_body = document.getElementById('cours_body');
     const mein = document.getElementById('mein');
     const buttonIds = ['log', 'cou', 'ser', 'car', 'rev', 'con', 'sup'];
+    const py_body = document.getElementById('python_body');
     const naim = document.getElementById('naim');
     const fot = document.getElementById('fot');
     const span = document.getElementById('span');
@@ -106,13 +46,13 @@ document.getElementById('span').addEventListener('click', function(){
             const btn = document.getElementById(id);
             if (btn) {
                 btn.classList.remove('no_dis');
-                btn.classList.add('vid');
+
             }
     });
 
     mein.classList.add('mein');
     mein.classList.remove('mein1');
-
+    py_body.classList.add('no_dis')
     cours_body.classList.add('no_dis');
     naim.classList.add('zug');
     naim.classList.remove('zug1');
@@ -130,8 +70,9 @@ document.getElementById('span').addEventListener('click', function(){
 
 document.getElementById('py_more').addEventListener('click', function(){
     const cours_body = document.getElementById('cours_body');
-    const py_body = document.getElementById('py_more');
+    const py_body = document.getElementById('python_body');
     const prow = window.getComputedStyle(py_body).display !== 'none';
+
     cours_body.classList.add('no_dis');
     py_body.classList.remove('no_dis');
 
---
+++ index/work.js
@@ -0,0 +1,58 @@
+/*Переключение слайдов регистрации*/
+
+document.getElementById('log').addEventListener('click', function() {
+    const log_button = document.getElementById('log');
+    const naim = document.getElementById('naim');
+    const log_body = document.getElementById('log_body');
+    const state = this.dataset.state;
+    const sig_body = document.getElementById('sig_body')
+
+    /*переключение кнопкт назад и войти*/
+
+    if (state === 'off'){
+        this.dataset.state = 'on'
+        naim.classList.add('no_vid');
+        log_body.classList.add('vid');
+        log_body.classList.remove('no_vid');
+        log_button.textContent = 'Remove';
+        this.style.background = '#ff4444';
+
+    }else {
+        this.style.background = '#11E200';
+        this.dataset.state = 'off';
+        this.textContent = 'login';
+        naim.classList.add('vid');
+        naim.classList.remove('no_vid');
+        log_body.classList.add('no_vid');
+        log_body.classList.remove('vid');
+        sig_body.classList.add('no_vid');
+        sig_body.classList.remove('vid');
+
+    }
+});
+
+/*кнопка регистрации в поле входа*/
+
+document.getElementById('log_ent').addEventListener('click', function() {
+    const log_body = document.getElementById('log_body');
+    const sig_body = document.getElementById('sig_body');
+    const state = this.dataset.state;
+
+        sig_body.classList.remove('vid');
+        sig_body.classList.add('no_vid');
+        log_body.classList.add('vid');
+        log_body.classList.remove('no_vid');
+
+
+
+});
+/*кнопка входа в поле регистрации*/
+document.getElementById('sig_ent').addEventListener('click', function() {
+    const sig_body = document.getElementById('sig_body');
+    const log_body = document.getElementById('log_body');
+
+        log_body.classList.remove('vid');
+        log_body.classList.add('no_vid');
+        sig_body.classList.remove('no_vid');
+        sig_body.classList.add('vid');
+});
