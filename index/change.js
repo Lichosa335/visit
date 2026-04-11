55c3bbcc77b5a81df6fcd627ccbe4a85acb5fd7b
Max <maxanc2009@gmail.com>
2026-04-11 18:53:16 +0800

    обновление сайта

--- index/change.js
+++ index/change.js
@@ -17,3 +17,11 @@ document.getElementById('py_more').addEventListener('click', function(){
     grid2.classList.add('no_dis');
 
 });
+document.getElementById('py_buy').addEventListener('click', function(){
+    const grid3 = document.getElementById('grid3');
+    const grid2= document.getElementById('grid2');
+
+    grid3.classList.remove('no_dis');
+    grid2.classList.add('no_dis');
+
+});
